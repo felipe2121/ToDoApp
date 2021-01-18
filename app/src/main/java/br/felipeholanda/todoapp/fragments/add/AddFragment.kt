1c66d396022c45d9abf532f2f9480f0e0e1ff69b
Felipe Alves Holanda <felipe.holanda12@yahoo.com.br>
2021-01-18 10:54:41 -0300

    - config navigation and fragments layouts.

---
+++ app/src/main/java/br/felipeholanda/todoapp/fragments/add/AddFragment.kt
@@ -0,0 +1,26 @@
+package br.felipeholanda.todoapp.fragments.add
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import br.felipeholanda.todoapp.R
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_add, container, false)
+
+        // Set Menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+}
---
+++ app/src/main/java/br/felipeholanda/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,26 @@
+package br.felipeholanda.todoapp.fragments.list
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import br.felipeholanda.todoapp.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // Set Menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+}
